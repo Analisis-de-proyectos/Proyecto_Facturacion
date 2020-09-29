1b2f073be59ac4e2ca5046d2b25ea77652a501e2
Abraham Argüello <int.arg1501@gmail.com>
2020-09-29 11:47:32 -0600

    Creacion del modelo basado en las tablas de la base de datos.

---
+++ Analisis_Proyecto_Facturacion/Analisis_Proyecto_Facturacion/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Analisis_Proyecto_Facturacion.Startup))]
+namespace Analisis_Proyecto_Facturacion
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
