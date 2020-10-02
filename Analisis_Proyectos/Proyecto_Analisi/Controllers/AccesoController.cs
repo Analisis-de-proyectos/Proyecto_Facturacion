3984eabd916feb104a34d0d0b9427fbe9aac49f9
Abraham Argüello <int.arg1501@gmail.com>
2020-10-01 23:45:24 -0600

    Nuevos avances con el Login y roles de usuarios

---
+++ Analisis_Proyectos/Proyecto_Analisi/Controllers/AccesoController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto_Analisi.Controllers
+{
+    public class AccesoController : Controller
+    {
+        // GET: Acceso
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string username,String password)
+        {
+            try
+            {
+
+                using (ventasEntities db = new ventasEntities() )
+                {
+
+                    var vUser = (from d in db.usuarios
+                                 where d.email == username.Trim() && d.password == password.Trim()
+                                 select d).FirstOrDefault();
+                    if (vUser == null)
+                    {
+                        ViewBag.Error = "Usuario o contraseña invalida";
+                        return View();
+                    }
+                    Session["User"] = vUser;
+                
+                }
+                    return RedirectToAction("Index", "Home");
+
+            }
+            catch (Exception e)
+
+            {
+
+                ViewBag.Error = e.Message;
+                return View();
+
+            }
+
+
+
+        }
+    }
+}
